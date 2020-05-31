55c4d82e958453eb9af953fd065283fc404e1d0e
Max Laverse <max@laverse.net>
2020-05-31 16:37:09 +0200

    add a simple test for configuration serialization

---
+++ src/test/java/org/jenkinsci/plugins/kubernetes/cli/MultiKubectlBuildWrapperTest.java
@@ -0,0 +1,68 @@
+package org.jenkinsci.plugins.kubernetes.cli;
+
+import com.cloudbees.plugins.credentials.CredentialsProvider;
+import com.cloudbees.plugins.credentials.CredentialsStore;
+import com.cloudbees.plugins.credentials.domains.Domain;
+import hudson.model.FreeStyleBuild;
+import hudson.model.FreeStyleProject;
+import hudson.model.Result;
+import hudson.tasks.Shell;
+import hudson.util.ListBoxModel;
+import org.jenkinsci.plugins.envinject.EnvInjectBuildWrapper;
+import org.jenkinsci.plugins.envinject.EnvInjectJobPropertyInfo;
+import org.jenkinsci.plugins.kubernetes.cli.helpers.DummyCredentials;
+import org.junit.Rule;
+import org.junit.Test;
+import org.jvnet.hudson.test.JenkinsRule;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+/**
+ * @author Max Laverse
+ */
+public class MultiKubectlBuildWrapperTest {
+    @Rule
+    public JenkinsRule r = new JenkinsRule();
+
+    @Test
+    public void testConfigurationPersistedOnSave() throws Exception {
+        CredentialsProvider.lookupStores(r.jenkins).iterator().next().addCredentials(Domain.global(), DummyCredentials.secretCredential("test-credentials"));
+
+        FreeStyleProject p = r.createFreeStyleProject();
+
+        KubectlCredential kc = new KubectlCredential();
+        kc.credentialsId= "test-credentials";
+        List<KubectlCredential> l = new ArrayList();
+        l.add(kc);
+        MultiKubectlBuildWrapper bw = new MultiKubectlBuildWrapper(l);
+        p.getBuildWrappersList().add(bw);
+
+        assertEquals("<?xml version='1.1' encoding='UTF-8'?>\n" +
+                "<project>\n" +
+                "  <keepDependencies>false</keepDependencies>\n" +
+                "  <properties/>\n" +
+                "  <scm class=\"hudson.scm.NullSCM\"/>\n" +
+                "  <canRoam>false</canRoam>\n" +
+                "  <disabled>false</disabled>\n" +
+                "  <blockBuildWhenDownstreamBuilding>false</blockBuildWhenDownstreamBuilding>\n" +
+                "  <blockBuildWhenUpstreamBuilding>false</blockBuildWhenUpstreamBuilding>\n" +
+                "  <triggers/>\n" +
+                "  <concurrentBuild>false</concurrentBuild>\n" +
+                "  <builders/>\n" +
+                "  <publishers/>\n" +
+                "  <buildWrappers>\n" +
+                "    <org.jenkinsci.plugins.kubernetes.cli.MultiKubectlBuildWrapper>\n" +
+                "      <kubectlCredentials>\n" +
+                "        <org.jenkinsci.plugins.kubernetes.cli.KubectlCredential>\n" +
+                "          <credentialsId>test-credentials</credentialsId>\n" +
+                "        </org.jenkinsci.plugins.kubernetes.cli.KubectlCredential>\n" +
+                "      </kubectlCredentials>\n" +
+                "    </org.jenkinsci.plugins.kubernetes.cli.MultiKubectlBuildWrapper>\n" +
+                "  </buildWrappers>\n" +
+                "</project>", p.getConfigFile().asString());
+    }
+}

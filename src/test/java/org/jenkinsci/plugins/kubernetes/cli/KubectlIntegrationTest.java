076463a898d1b4b8f422c5b6394ea47156641d05
Max Laverse <max@laverse.net>
2022-05-22 11:40:16 +0200

    separate unit and integration tests

--- src/test/java/org/jenkinsci/plugins/kubernetes/cli/KubectlIntegrationTest.java
+++ src/test/java/org/jenkinsci/plugins/kubernetes/cli/KubectlIntegrationTest.java
@@ -14,6 +14,7 @@ import org.jenkinsci.plugins.workflow.job.WorkflowRun;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.experimental.categories.Category;
 import org.jvnet.hudson.test.JenkinsRule;
 
 import java.io.File;
@@ -32,6 +33,7 @@ import static org.junit.Assume.assumeTrue;
 /**
  * @author Max Laverse
  */
+@Category(KubectlIntegrationTest.class)
 public class KubectlIntegrationTest {
     @Rule
     public JenkinsRule r = new JenkinsRule();
@@ -41,7 +43,7 @@ public class KubectlIntegrationTest {
 
     @Before
     public void checkKubectlPresence() {
-        assumeTrue("The '" + kubectlBinaryName() + "' binary could not be found in the PATH", kubectlPresent());
+        assertThat("The '" + kubectlBinaryName() + "' binary could not be found in the PATH", kubectlPresent());
     }
 
     @Test
